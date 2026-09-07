a0a2b7f67f20032ea14c0834274e4bfbfd8ce17d
JazzCZ91 <d.suchanek91@seznam.cz>
2026-09-07 19:01:54 +0200

    Update service worker to use new cache version

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = "kniha-jizd-v1";
+const CACHE_NAME = "kniha-jizd-v3";
 
-const FILES_TO_CACHE = [
+const FILES = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
 });
 
 self.addEventListener("activate", event => {
@@ -24,16 +23,22 @@ self.addEventListener("activate", event => {
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
       )
-    )
+    ).then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
